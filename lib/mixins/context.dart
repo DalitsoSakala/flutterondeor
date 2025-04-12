34bac2bbf15e0e03275231d64fc2694b2e0cc6bc
Dalitso Sakala <dalitso.1sc@gmail.com>
2025-04-12 17:00:53 +0200

    - Added Mediadata

--- lib/mixins/context.dart
+++ lib/mixins/context.dart
@@ -4,5 +4,6 @@ mixin ContextValuesMixin<T extends StatefulWidget> on State<T>{
   ThemeData get theme_=>Theme.of(context);
   TextTheme? get textTheme_=> theme_.textTheme;
   ColorScheme? get colorScheme_=>theme_.colorScheme;
+  MediaQueryData get media_=>MediaQuery.of(context);
   Size get mediaSize_=>MediaQuery.sizeOf(context);
 }
